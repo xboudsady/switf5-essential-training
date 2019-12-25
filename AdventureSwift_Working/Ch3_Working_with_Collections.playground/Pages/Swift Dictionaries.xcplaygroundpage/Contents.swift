89b640cbaf1f6e8dff362339f225899523e06835
xboudsady <xboudsady@xayaseths-mbp.lan>
2019-12-24 23:45:13 -0800

    Swift Dictionaries modified

--- AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
@@ -20,6 +20,10 @@ var blacksmithShop: [String: Int] = ["Bottle": 10, "Shield": 15, "Ocarina": 1000
 // Accessing and modifying values
 var shieldPrice = blacksmithShop["Shield"]
 blacksmithShop["Bottle"] = 11
+print(blacksmithShop)
 
 // All keys and values
+let allKeys = [String](blacksmithShop.keys)
+
+let allValues = [Int](blacksmithShop.values)
 
