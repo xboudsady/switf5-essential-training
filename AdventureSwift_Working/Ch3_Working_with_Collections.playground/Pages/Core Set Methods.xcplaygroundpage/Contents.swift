f6d96fae451c9625470dde073f7c168dd3110989
xboudsady <xboudsady@xayaseths-mbp.lan>
2019-12-25 00:10:43 -0800

    Modified Core Set Methods playground.

--- AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Core Set Methods.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch3_Working_with_Collections.playground/Pages/Core Set Methods.xcplaygroundpage/Contents.swift
@@ -14,7 +14,12 @@
 
  */
 // Test variables
-
+var activeQuests: Set = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny Needs Firewood"]
+var completedQuests: Set = ["Big Boss", "All-4-One", "The Hereafter"]
 
 // Set operations
+var commonQuests = activeQuests.intersection(completedQuests)
+var differentQuests = activeQuests.symmetricDifference(completedQuests)
+var allQuests = activeQuests.union(completedQuests)
+var clippedQuests = activeQuests.subtracting((completedQuests))
 
