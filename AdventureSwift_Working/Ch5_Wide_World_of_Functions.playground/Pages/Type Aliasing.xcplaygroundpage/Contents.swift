f2f181473ac6e8b4212144cafddeecb60d45b4d5
xboudsady <boudsadyx@gmail.com>
2019-12-25 20:28:22 -0800

    Completed Type Aliasing playground

--- AdventureSwift_Working/Ch5_Wide_World_of_Functions.playground/Pages/Type Aliasing.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch5_Wide_World_of_Functions.playground/Pages/Type Aliasing.xcplaygroundpage/Contents.swift
@@ -18,9 +18,21 @@
 var partyMembers = ["Harrison", "Steven", "Bob the Noob"]
 
 // Type alias as a return value
+typealias AttackTuple = (name: String, damage: Int, rechargeable: Bool)
+
+var sunStrike: AttackTuple = ("Sun Strike", 45, false)
+
+func levelUpAttack(baseAttack: AttackTuple) -> AttackTuple {
+    let increasedAttack: AttackTuple = (baseAttack.name, baseAttack.damage + 10, true)
+    return increasedAttack
+}
+
+var poweredAttack = levelUpAttack(baseAttack: sunStrike)
 
 
 // Type alias as a function parameter
+typealias ArrayClosure = ([String]) -> Void
+
 func activeMembers(completion: ([String]) -> Void) {
     completion(partyMembers)
 }
