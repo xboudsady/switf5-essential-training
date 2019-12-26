706440dedba27d5d301b00a901ee95be9575f716
xboudsady <boudsadyx@gmail.com>
2019-12-26 15:18:28 -0800

    Completed 'Chaining Optionals' playground.

--- AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Chaining optionals.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Chaining optionals.xcplaygroundpage/Contents.swift
@@ -12,18 +12,23 @@
 
  */
 // Test classes
-struct Item {
-    var description: String
-    var previousOwner: Owner?
+struct Item {                           // Declaure our Struct
+    var description: String             // Variable, String data type
+    var previousOwner: Owner?           // Variable, Optional
 }
 
-struct Owner {
-    var name: String
+struct Owner {                          // Declare our Struct
+    var name: String                    // Variable, String data type
+    
+    func returnOwnerInfo() -> String {              // Function with String output
+        return "\(name) is the current owner!"
+    }
 }
 
-var questDirectory = [
+// Nested Dictionary
+var questDirectory = [                              // Outer Dictionary
     "Fetch Gemstones": [
-        "Objective": "Retrieve 5 gemstones",
+        "Objective": "Retrieve 5 gemstones",        // Inner Dictionary
         "Secret": "Complete in under 5 minutes"
     ],
     "Defeat Big Boss": [
@@ -33,4 +38,33 @@ var questDirectory = [
 ]
 
 // Creating the chain
+// Create an instances of our Item, Struct
+var rareDagger = Item(description: "A unique dagger of unkown origin", previousOwner: nil)
 
+var daggerOwner = Owner(name: "The Great Thief")
+
+// Apply a value to previous Owner property
+rareDagger.previousOwner = daggerOwner
+
+// Manually add name property
+//rareDagger.previousOwner?.name = "The Old Merchant"
+
+
+// Unwrapping our optional, in our Item, Struct
+// Added question mark is telling compilier to return nil, if no previousOwner value is nil
+if let owner = rareDagger.previousOwner?.name {
+    print("This item used to be owned by \(owner)")
+} else {
+    print("Loks like this items history is unknown...")
+}
+
+// Check if Owner exist
+if let ownerInfo = rareDagger.previousOwner?.returnOwnerInfo() {
+    print("Owner found!")
+} else {
+    print("No owner in our records...")
+}
+
+if let gemstoneObjective = questDirectory["Fetch Gemstones"]?["Objective"] {
+    print(gemstoneObjective)
+}
