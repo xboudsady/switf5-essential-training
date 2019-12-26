08949551d9fc4de902ac155632f95e1002dc87a8
xboudsady <boudsadyx@gmail.com>
2019-12-26 14:27:10 -0800

    Completed 'Basic Swift Structs' playground.

--- AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift structs.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift structs.xcplaygroundpage/Contents.swift
@@ -15,6 +15,42 @@
 
  */
 // Declaring a new struct
+struct Level {
+    let levelID: Int                            // Variable, with data type Int
+    var levelObjectives: [String]               // Variable, with data type Array of Strings
+    
+    var levelDescription: String {
+        return "Level ID: \(self.levelID)"
+    }
+    
+    init(id: Int, objectives: [String]) {       // Initialize function for our class
+        self.levelID = id                       // First argument as Int
+        self.levelObjectives = objectives       // Second argument as Array of Strings
+    }
+    
+    func queryObjectives() {                    // Function to loop through our
+        for objective in levelObjectives {
+            print("\(objectives)")
+        }
+    }
+    
+    mutating func completeObjective(index: Int) {
+        levelObjectives.remove(at: index)
+    }
+}
+
+// Array of Strings
+var objectives = ["Find the lost cat", "Collect all gemstones", "Defeat the Big Boss"]
+
+// Create an instances of our Level Struct
+// 1st argument, Int; 2nd argument, Array of Strings
+var level1 = Level(id: 1, objectives: objectives)
 
 
 // Value types again
+var defaultLevel = level1
+
+level1.completeObjective(index: 0)
+level1.queryObjectives()
+
+defaultLevel.queryObjectives()
