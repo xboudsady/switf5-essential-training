6afd8919f818eddbbd608f985750ce97f45509cf
xboudsady <boudsadyx@gmail.com>
2019-12-25 20:36:46 -0800

    Completed Ch5 Challenge

--- Ch05/Ch05_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
+++ Ch05/Ch05_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
@@ -15,16 +15,36 @@
  
  */
 // 1
+typealias Attack = (name: String, damage: Int)
 
 // 2
+func attackEnemy(damage: Int) {
+    print("Enemy hit for \(damage) damage!\n")
+}
 
 // 3
+func attackEnemy(attack: Attack) -> String {
+    return "You attack with \(attack.name)\nEnemy hit for \(attack.damage) damage!\n"
+}
 
 // 4
+attackEnemy(damage: 90)
+var message = attackEnemy(attack: ("Power Slam", 55))
+print(message)
 
 // 5
+typealias AttackClosure = ([Attack]) -> Void
+let playerActions = [("Mist Bomb", 45), ("Rangers Eye", 15), ("Fists of Fury", 5)]
 
 // 6
+func fetchPlayerAttacks(closure: AttackClosure) {
+    closure(playerActions)
+}
 
 // 7
+fetchPlayerAttacks { (attackOptions) in
+    for (name, damage) in attackOptions {
+        print("\(name) will hit for \(damage) if cast...")
+    }
+}
 
