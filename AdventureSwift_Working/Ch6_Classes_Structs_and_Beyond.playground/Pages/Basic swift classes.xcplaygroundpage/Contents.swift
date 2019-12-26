63ba590cf7f0e7c33b77a6083fc49ff878689f2f
xboudsady <boudsadyx@gmail.com>
2019-12-26 10:43:23 -0800

    Completed 'Basic Swift Classes' playground.

--- AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift classes.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift classes.xcplaygroundpage/Contents.swift
@@ -15,4 +15,35 @@
  
  */
 // Declaring a new class
+class Adventurer {
+    var name: String
+    let maxHealth: Int
+    
+    var specialMove: String?
+    
+    init(name: String, maxHP: Int) {
+        self.name = name
+        self.maxHealth = maxHP
+    }
+    
+    convenience init(name: String) {
+        self.init(name: name, maxHP: 100)
+    }
+    
+    func printStats() {
+        print("Character: \(self.name), MaxHealth: \(self.maxHealth)")
+    }
+}
+
+var player1 = Adventurer(name: "Harrison", maxHP: 99)
+var player2 = Adventurer(name: "Steven")
+var defaultPlayer = player1
+defaultPlayer.name = "Bob the Noob"
+player1.name = "Harrison"
+
+player1.printStats()
+player2.printStats()
+
+
+
 
