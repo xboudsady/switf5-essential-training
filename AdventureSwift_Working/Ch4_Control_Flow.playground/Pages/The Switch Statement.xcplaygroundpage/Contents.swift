abef0ae2166d8bbb96dfc871734bb1b5a250fde7
xboudsady <boudsadyx@gmail.com>
2019-12-25 18:58:22 -0800

    Completed The Switch Statement playground.

--- AdventureSwift_Working/Ch4_Control_Flow.playground/Pages/The Switch Statement.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch4_Control_Flow.playground/Pages/The Switch Statement.xcplaygroundpage/Contents.swift
@@ -15,11 +15,27 @@
  */
 // Test variables
 let initial = "H"
-let hp = 26
-let mp = 24
+let hp = 30
+let mp = 40
 
 // Simple switch
-
+switch initial {
+case "H":
+    print("I'm guessing Harold?")
+case "A":
+    print("Maybe Alita?")
+default:
+    print("I've got nothing...")
+}
 
 // Complex variations
-
+switch (mp, hp) {
+case (15, 10):
+    print("Great job")
+case (1...15, 20..<25):
+    print("Ranges are the best!")
+case (let localMP, let localHP) where localMP + localHP > 20:
+    print(localMP, localHP)
+default:
+    print("I've got nothing...")
+}
