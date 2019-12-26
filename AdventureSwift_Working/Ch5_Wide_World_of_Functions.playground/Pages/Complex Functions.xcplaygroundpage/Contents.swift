0ef8ca7332c21193c9bb4ce1970673a80fdf0dc4
xboudsady <boudsadyx@gmail.com>
2019-12-25 19:40:06 -0800

    Completed Complex Functions playground.

--- AdventureSwift_Working/Ch5_Wide_World_of_Functions.playground/Pages/Complex Functions.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch5_Wide_World_of_Functions.playground/Pages/Complex Functions.xcplaygroundpage/Contents.swift
@@ -14,10 +14,31 @@
 
  */
 // Optional return value
+func setupArenaMatch() -> Bool? {
+    return nil
+}
 
+if let initSuccess = setupArenaMatch() {
+    print("Level initialized: \(initSuccess)")
+} else {
+    print("Something went terribly wrong...")
+}
 
 // Multiple return values
+func setupArenaMatch(levelName: String) -> (success: Bool, secretItem: String) {
+    print("\(levelName) initialized...")
+    return (true, "Minotaur's Head")
+}
+
+var levelDetails = setupArenaMatch(levelName: "Poison Flats")
+levelDetails.secretItem
+levelDetails.success
 
 
 // Default values
+func setupDefaultMatch(levelName: String = "Fire Marshes", opponents: Int = 3) {
+    print("Arena match will take place in the \(levelName) between \(opponents) players!")
+}
 
+setupDefaultMatch()
+setupDefaultMatch(levelName: "Poison Flats", opponents: 10)
