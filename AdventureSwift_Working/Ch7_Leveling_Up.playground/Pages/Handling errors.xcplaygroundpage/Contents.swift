1fc90baa934ed0353f98a0183b11e6f725a995ed
xboudsady <boudsadyx@gmail.com>
2019-12-27 14:19:45 -0800

    Completed 'Handling Error' playground.'

--- AdventureSwift_Working/Ch7_Leveling_Up.playground/Pages/Handling errors.xcplaygroundpage/Contents.swift
+++ AdventureSwift_Working/Ch7_Leveling_Up.playground/Pages/Handling errors.xcplaygroundpage/Contents.swift
@@ -18,8 +18,9 @@ enum DataError: Error {
     case InvalidPath
 }
 
+let playerDataPath = "/example/data.txt"
 
-func loadData(path: String) throws {
+func loadData(path: String) throws -> Bool? {
     guard path.contains("/") else {
         throw DataError.InvalidPath
     }
@@ -27,9 +28,36 @@ func loadData(path: String) throws {
     guard !path.isEmpty else {
         throw DataError.EmptyPath
     }
+    
+    return true
 }
 
 // Do-Catch statements
+do {
+    try loadData(path: playerDataPath)
+    print("Data fetch successful!")
+} catch is DataError {
+    print("Invalid or empty path detected...")
+} catch {
+    print("Unknown error detected...")
+}
 
+if let dataLoaded = try loadData(path: playerDataPath) {
+    print("Data fetch went just fine...")
+}
 
 // Propagating errors
+func propagateDataError() throws {
+    try loadData(path: playerDataPath)
+}
+
+do {
+    try propagateDataError()
+    print("Propagated data fetch successful!")
+} catch DataError.EmptyPath {
+    print("Empty path detected...")
+} catch DataError.InvalidPath {
+    print("Invalid path detected...")
+} catch {
+    print("Unkown error...")
+}
